0cb028e6303469d43d829999c058c1fc692f6631
Saurabh Kumar <146050167+Saurabh-Kumar20-24@users.noreply.github.com>
2025-04-06 16:34:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
+	s1.toLowerCase();
+	s2.toLowerCase();
+	for(let i=0; i<=s1.length-s2.length; i++){
+		if(s1.slice(i,s1.length)===s2){
+			return i;
+		}
+	}
 }
 
 // Please do not change the code below
